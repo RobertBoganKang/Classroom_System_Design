692da4ef6b38642175089163eb9ee1f3b108abef
Robert Bogan Kang <bob.kang@qq.com>
2017-11-16 01:43:42 -0600

    fix mobile view
    Adjust its layout on mobile fixed

--- src/teacherClassroom/teacherHeader.php
+++ src/teacherClassroom/teacherHeader.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/reset.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
